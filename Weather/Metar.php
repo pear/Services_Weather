80bcb43334e7787ebcfd9f3b1b7ac4a4beb06024
Alexander Wirtz <eru@php.net>
2005-11-13 22:44:47 +0000

    Changed default condition for determining icon
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Weather/trunk@200457 c90b9560-bf6c-de11-be94-00142212c4b1

--- Weather/Metar.php
+++ Weather/Metar.php
@@ -1740,12 +1740,12 @@ class Services_Weather_Metar extends Services_Weather_Common
 
             // Add an icon for the current conditions
             // Determine if certain values are set, if not use defaults
-            $condition   = isset($weatherReturn["condition"])   ? $weatherReturn["condition"]   :      "";
-            $clouds      = isset($weatherReturn["clouds"])      ? $weatherReturn["clouds"]      : array();
-            $wind        = isset($weatherReturn["wind"])        ? $weatherReturn["wind"]        :       5; 
-            $temperature = isset($weatherReturn["temperature"]) ? $weatherReturn["temperature"] :      70; 
-            $latitude    = isset($location["latitude"])         ? $location["latitude"]         :    -360;
-            $longitude   = isset($location["longitude"])        ? $location["longitude"]        :    -360;
+            $condition   = isset($weatherReturn["condition"])   ? $weatherReturn["condition"]   : "No Significant Weather";
+            $clouds      = isset($weatherReturn["clouds"])      ? $weatherReturn["clouds"]      :                  array();
+            $wind        = isset($weatherReturn["wind"])        ? $weatherReturn["wind"]        :                        5; 
+            $temperature = isset($weatherReturn["temperature"]) ? $weatherReturn["temperature"] :                       70; 
+            $latitude    = isset($location["latitude"])         ? $location["latitude"]         :                     -360;
+            $longitude   = isset($location["longitude"])        ? $location["longitude"]        :                     -360;
             
             // Get the icon
             $weatherReturn["conditionIcon"] = $this->getWeatherIcon($condition, $clouds, $wind, $temperature, $latitude, $longitude);
