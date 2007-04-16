a875c9fad50737f507723aceb40faa7bd7434a31
Alexander Wirtz <eru@php.net>
2007-04-16 10:36:05 +0000

    Upgrade Copyright
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Weather/trunk@233866 c90b9560-bf6c-de11-be94-00142212c4b1

--- Weather.php
+++ Weather.php
@@ -13,7 +13,7 @@
  * PHP versions 4 and 5
  *
  * <LICENSE>
- * Copyright (c) 2005-2006, Alexander Wirtz
+ * Copyright (c) 2005-2007, Alexander Wirtz
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -44,7 +44,7 @@
  * @category    Web Services
  * @package     Services_Weather
  * @author      Alexander Wirtz <alex@pc4p.net>
- * @copyright   2005-2006 Alexander Wirtz
+ * @copyright   2005-2007 Alexander Wirtz
  * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version     CVS: $Id$
  * @link        http://pear.php.net/package/Services_Weather
@@ -90,7 +90,7 @@ define("SERVICES_WEATHER_ERROR_INVALID_LICENSE_KEY",    102);
  * @category    Web Services
  * @package     Services_Weather
  * @author      Alexander Wirtz <alex@pc4p.net>
- * @copyright   2005-2006 Alexander Wirtz
+ * @copyright   2005-2007 Alexander Wirtz
  * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version     Release: @package_version@
  * @link        http://pear.php.net/package/Services_Weather
